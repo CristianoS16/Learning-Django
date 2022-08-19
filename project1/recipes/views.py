f5ce84c3b675cdc082c66210941c2d64d6a04f0d
Cristiano <cristianosampaio16@gmail.com>
2022-08-19 19:33:57 -0300

    clean files

--- project1/recipes/views.py
+++ project1/recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -6,11 +5,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'João',
     })
-
-
-def contato(request):
-    return render(request, 'temp/temp.html')
-
-
-def sobre(request):
-    return HttpResponse("sobre")
