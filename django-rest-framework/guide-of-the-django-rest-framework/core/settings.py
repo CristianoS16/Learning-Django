a4f0b2aae885f7f4d27acc7099675a4c5b9f5c9e
Cristiano <cristianosampaio16@gmail.com>
2022-09-17 20:32:13 -0300

    add pagination

--- django-rest-framework/guide-of-the-django-rest-framework/core/settings.py
+++ django-rest-framework/guide-of-the-django-rest-framework/core/settings.py
@@ -136,4 +136,8 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ),
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.LimitOffsetPagination',
+
+    'PAGE_SIZE': 100,
 }
