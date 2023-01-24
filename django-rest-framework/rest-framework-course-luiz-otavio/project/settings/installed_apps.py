acacda872d110c229e9ef782cd1c7f4b39d924d8
Cristiano <cristianosampaio16@gmail.com>
2023-01-23 23:01:15 -0300

    add drf to installed apps

--- django-rest-framework/rest-framework-course-luiz-otavio/project/settings/installed_apps.py
+++ django-rest-framework/rest-framework-course-luiz-otavio/project/settings/installed_apps.py
@@ -1,11 +1,14 @@
 INSTALLED_APPS = [
+    # Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # A gente tem apps também
+    # Django rest framework
+    'rest_framework'
+    # My apps
     'recipes',
     'authors',
     'tag',
