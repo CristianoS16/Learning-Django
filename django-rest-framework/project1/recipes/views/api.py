b1ff48710b1cf1291f8e19d3794e4732c2c5f991
Cristiano <cristianosampaio16@gmail.com>
2023-01-27 08:27:38 -0300

    add post serializer without validation

--- django-rest-framework/project1/recipes/views/api.py
+++ django-rest-framework/project1/recipes/views/api.py
@@ -16,8 +16,14 @@ def recipe_api_list(request):
         serializer = RecipeSerializer(
             instance=recipes, many=True, context={'request': request})
         return Response(serializer.data)
+
     elif request.method == "POST":
-        return Response("POST", status=status.HTTP_201_CREATED)
+        serializer = RecipeSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        # serializer.save()
+        return Response(
+            serializer.validated_data, status=status.HTTP_201_CREATED
+        )
 
 
 @api_view()
