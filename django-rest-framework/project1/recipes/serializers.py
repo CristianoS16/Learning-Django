86e8ce6eadf9f07bb33a09390263413f3cdce5c5
Cristiano <cristianosampaio16@gmail.com>
2023-01-28 16:47:22 -0300

    add delete and patch methods

--- django-rest-framework/project1/recipes/serializers.py
+++ django-rest-framework/project1/recipes/serializers.py
@@ -45,6 +45,12 @@ class RecipeSerializer(serializers.ModelSerializer):
         return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
 
     def validate(self, attrs):
+
+        if self.instance is not None and attrs.get('servings') is None:
+            attrs['servings'] = self.instance.servings
+        if self.instance is not None and attrs.get('preparation_time') is None:
+            attrs['preparation_time'] = self.instance.preparation_time
+
         super_clean = super().validate(attrs)
         AuthorRecipeValidator(
             data=attrs, ErrorClass=serializers.ValidationError)
--- django-rest-framework/project1/recipes/views/api.py
+++ django-rest-framework/project1/recipes/views/api.py
@@ -27,23 +27,43 @@ def recipe_api_list(request):
         )
 
 
-@api_view()
+@api_view(http_method_names=['get', 'patch', 'delete'])
 def recipe_api_detail(request, pk):
+
     recipe = get_object_or_404(
         Recipe.objects.get_published(),
         pk=pk
     )
-    serializer = RecipeSerializer(
-        instance=recipe, many=False, context={'request': request})
-    return Response(serializer.data)
-    # recipe = Recipe.objects.get_published().filter(pk=pk).first()
-    # if recipe:
-    #     serializer = RecipeSerializer(instance=recipe, many=False)
-    #     return Response(serializer.data)
-    # else:
-    #     return Response({
-    #         "detail": "404 Bad Request"
-    #     }, status=status.HTTP_418_IM_A_TEAPOT)
+
+    if request.method == 'GET':
+        serializer = RecipeSerializer(
+            instance=recipe, many=False, context={'request': request})
+        return Response(serializer.data)
+        # recipe = Recipe.objects.get_published().filter(pk=pk).first()
+        # if recipe:
+        #     serializer = RecipeSerializer(instance=recipe, many=False)
+        #     return Response(serializer.data)
+        # else:
+        #     return Response({
+        #         "detail": "404 Bad Request"
+        #     }, status=status.HTTP_418_IM_A_TEAPOT)
+
+    elif request.method == 'PATCH':
+        serializer = RecipeSerializer(
+            instance=recipe,
+            data=request.data,
+            many=False,
+            context={'request': request},
+            partial=True
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        recipe.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 @api_view()
