1e89d7200c95e7c71cef2449fb96b3092573db8d
Cristiano <cristianosampaio16@gmail.com>
2023-01-30 19:23:54 -0300

    add ListCreateAPIView instead APIView

--- django-rest-framework/project1/recipes/views/api.py
+++ django-rest-framework/project1/recipes/views/api.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
+from rest_framework.generics import ListCreateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -10,21 +11,23 @@ from ..models import Recipe
 from ..serializers import RecipeSerializer, TagSerializer
 
 
-class RecipeAPIv2List(APIView):
-    def get(self, request):
-        recipes = Recipe.objects.get_published()[:10]
-        serializer = RecipeSerializer(
-            instance=recipes, many=True, context={'request': request})
-        return Response(serializer.data)
+class RecipeAPIv2List(ListCreateAPIView):
+    queryset = Recipe.objects.get_published()
+    serializer_class = RecipeSerializer
+    # def get(self, request):
+    #     recipes = Recipe.objects.get_published()[:10]
+    #     serializer = RecipeSerializer(
+    #         instance=recipes, many=True, context={'request': request})
+    #     return Response(serializer.data)
 
-    def post(self, request):
-        serializer = RecipeSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(
-            serializer.data,
-            status=status.HTTP_201_CREATED
-        )
+    # def post(self, request):
+    #     serializer = RecipeSerializer(data=request.data)
+    #     serializer.is_valid(raise_exception=True)
+    #     serializer.save()
+    #     return Response(
+    #         serializer.data,
+    #         status=status.HTTP_201_CREATED
+    #     )
 
 
 class RecipeAPIv2Detail(APIView):
