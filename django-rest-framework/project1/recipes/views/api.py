15813dcd052671018736d08fecf9429890871b5c
Cristiano <cristianosampaio16@gmail.com>
2023-01-31 09:02:56 -0300

    add pagination

--- django-rest-framework/project1/recipes/views/api.py
+++ django-rest-framework/project1/recipes/views/api.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.generics import ListCreateAPIView
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -11,9 +12,14 @@ from ..models import Recipe
 from ..serializers import RecipeSerializer, TagSerializer
 
 
+class RecipeAPIv2Pagination(PageNumberPagination):
+    page_size = 2
+
+
 class RecipeAPIv2List(ListCreateAPIView):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
+    pagination_class = RecipeAPIv2Pagination
     # def get(self, request):
     #     recipes = Recipe.objects.get_published()[:10]
     #     serializer = RecipeSerializer(
