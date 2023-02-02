e5597bcd9ec02f55d58ae21fe96b0fa869b3da66
Cristiano <cristianosampaio16@gmail.com>
2023-02-02 08:21:02 -0300

    aply modelviewset

--- django-rest-framework/project1/recipes/urls.py
+++ django-rest-framework/project1/recipes/urls.py
@@ -47,12 +47,16 @@ urlpatterns = [
     ),
     path(
         'recipes/api/v2/',
-        views.RecipeAPIv2List.as_view(),
+        views.RecipeAPIv2ViewSet.as_view({
+            'get': 'list', 'post': 'create',
+        }),
         name="recipe_api_v2",
     ),
     path(
         'recipes/api/v2/<int:pk>/',
-        views.RecipeAPIv2Detail.as_view(),
+        views.RecipeAPIv2ViewSet.as_view({
+            'get': 'retrieve', 'patch': 'partial_update', 'delete': 'destroy',
+        }),
         name="recipe_api_v2_detail",
     ),
     path(
--- django-rest-framework/project1/recipes/views/api.py
+++ django-rest-framework/project1/recipes/views/api.py
@@ -1,9 +1,8 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
-from rest_framework.generics import (ListCreateAPIView,
-                                     RetrieveUpdateDestroyAPIView)
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
 from tag.models import Tag
 
@@ -15,32 +14,13 @@ class RecipeAPIv2Pagination(PageNumberPagination):
     page_size = 2
 
 
-class RecipeAPIv2List(ListCreateAPIView):
-    queryset = Recipe.objects.get_published()
-    serializer_class = RecipeSerializer
-    pagination_class = RecipeAPIv2Pagination
-    # def get(self, request):
-    #     recipes = Recipe.objects.get_published()[:10]
-    #     serializer = RecipeSerializer(
-    #         instance=recipes, many=True, context={'request': request})
-    #     return Response(serializer.data)
-
-    # def post(self, request):
-    #     serializer = RecipeSerializer(data=request.data)
-    #     serializer.is_valid(raise_exception=True)
-    #     serializer.save()
-    #     return Response(
-    #         serializer.data,
-    #         status=status.HTTP_201_CREATED
-    #     )
-
-
-class RecipeAPIv2Detail(RetrieveUpdateDestroyAPIView):
+class RecipeAPIv2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
 
     # Overwrite partial_update from django rest framework
+
     def partial_update(self, request, *args, **kwargs):
         pk = kwargs.get('pk')
         recipe = self.get_queryset().filter(pk=pk).first()
@@ -54,6 +34,47 @@ class RecipeAPIv2Detail(RetrieveUpdateDestroyAPIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data,)
+
+
+# class RecipeAPIv2List(ListCreateAPIView):
+#     queryset = Recipe.objects.get_published()
+#     serializer_class = RecipeSerializer
+#     pagination_class = RecipeAPIv2Pagination
+#     # def get(self, request):
+#     #     recipes = Recipe.objects.get_published()[:10]
+#     #     serializer = RecipeSerializer(
+#     #         instance=recipes, many=True, context={'request': request})
+#     #     return Response(serializer.data)
+
+#     # def post(self, request):
+#     #     serializer = RecipeSerializer(data=request.data)
+#     #     serializer.is_valid(raise_exception=True)
+#     #     serializer.save()
+#     #     return Response(
+#     #         serializer.data,
+#     #         status=status.HTTP_201_CREATED
+#     #     )
+
+
+# class RecipeAPIv2Detail(RetrieveUpdateDestroyAPIView):
+#     queryset = Recipe.objects.get_published()
+#     serializer_class = RecipeSerializer
+#     pagination_class = RecipeAPIv2Pagination
+
+#     # Overwrite partial_update from django rest framework
+#     def partial_update(self, request, *args, **kwargs):
+#         pk = kwargs.get('pk')
+#         recipe = self.get_queryset().filter(pk=pk).first()
+#         serializer = RecipeSerializer(
+#             instance=recipe,
+#             data=request.data,
+#             many=False,
+#             context={'request': request},
+#             partial=True
+#         )
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data,)
 
 
 @api_view()
