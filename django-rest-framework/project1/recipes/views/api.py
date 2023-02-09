af1c07fd55b7710669e6068e677c1a947630b9c1
Cristiano <cristianosampaio16@gmail.com>
2023-02-08 23:47:10 -0300

    add my own permission

--- django-rest-framework/project1/recipes/views/api.py
+++ django-rest-framework/project1/recipes/views/api.py
@@ -1,13 +1,14 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from tag.models import Tag
 
 from ..models import Recipe
+from ..permissions import IsOwner
 from ..serializers import RecipeSerializer, TagSerializer
 
 
@@ -19,7 +20,7 @@ class RecipeAPIv2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
-    permission_classes = [IsAuthenticated, ]
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
 
     def get_serializer_class(self):
         return super().get_serializer_class()
@@ -42,11 +43,27 @@ class RecipeAPIv2ViewSet(ModelViewSet):
 
         return qs
 
+    def get_object(self):
+        pk = self.kwargs.get("pk", '')
+        obj = get_object_or_404(
+            self.get_queryset(),
+            pk=pk
+        )
+
+        self.check_object_permissions(self.request, obj)
+
+        return obj
+
+    def get_permissions(self):
+
+        if self.request.method in ['PATCH', 'DELETE']:
+            return [IsOwner(), ]
+        return super().get_permissions()
+
     # Overwrite partial_update from django rest framework
 
     def partial_update(self, request, *args, **kwargs):
-        pk = kwargs.get('pk')
-        recipe = self.get_queryset().filter(pk=pk).first()
+        recipe = self.get_object()
         serializer = RecipeSerializer(
             instance=recipe,
             data=request.data,
