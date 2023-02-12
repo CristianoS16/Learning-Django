a231f89cd1145658119db0e9fe334b59bcff8174
Cristiano <cristianosampaio16@gmail.com>
2023-02-12 19:05:34 -0300

    add test_recipe_api_list_returns_status_code_200

--- django-rest-framework/project1/recipes/tests/test_recipe_api.py
+++ django-rest-framework/project1/recipes/tests/test_recipe_api.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from rest_framework import test
 
 from recipes.tests.test_recipe_base import RecipeMixin
@@ -5,5 +6,13 @@ from recipes.tests.test_recipe_base import RecipeMixin
 
 class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
 
-    def test_the_test(self):
-        assert 1 == 1
+    def test_recipe_api_list_returns_status_code_200(self):
+
+        api_url = reverse('recipes:recipes-api-list')
+
+        response = self.client.get(api_url)
+
+        self.assertEqual(
+            response.status_code,
+            200
+        )
