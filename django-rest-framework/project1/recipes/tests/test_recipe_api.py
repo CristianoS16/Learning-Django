bbf35b4662a533354ff4c6807220c88ec8963908
Cristiano <cristianosampaio16@gmail.com>
2023-02-13 08:07:39 -0300

    add test_recipe_api_list_loads_correct_number_of_recipes

--- django-rest-framework/project1/recipes/tests/test_recipe_api.py
+++ django-rest-framework/project1/recipes/tests/test_recipe_api.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from django.urls import reverse
 from rest_framework import test
 
@@ -15,4 +17,16 @@ class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
         self.assertEqual(
             response.status_code,
             200
+        )
+
+    @patch('recipes.views.api.RecipeAPIv2Pagination.page_size', new=7)
+    def test_recipe_api_list_loads_correct_number_of_recipes(self):
+        wanted_recipes = 7
+        self.make_recipe_in_batch(qtd=wanted_recipes)
+        response = self.client.get(
+            reverse('recipes:recipes-api-list')+'?page=1')
+        qtd_recipes_received = len(response.data.get('results'))
+        self.assertEqual(
+            wanted_recipes,
+            qtd_recipes_received
         )
